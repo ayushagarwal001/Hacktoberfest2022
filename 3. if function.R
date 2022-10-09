02f308939bc15e6a14ff1a92862555c16bceb501
Su-001 <110022483+Su-001@users.noreply.github.com>
2022-10-09 16:01:14 +0530

    Add files via upload

---
+++ 3. if function.R
@@ -0,0 +1,30 @@
+#if function-------------------------------------------------
+
+#if(logical statement){value if true}
+
+a <- 5
+if(a > 2){"a is greater than 2"}
+
+a <- 1
+if(a > 2){"a is greater than 2"}
+
+
+#if(logical statement){value if true}else{value if false}
+
+a <- 1
+if(a > 2){"a is greater than 2"}else{"a is less than 2"}
+
+#ifelse(logical condition,"value if true",value if false)
+
+a < -1
+ifelse(a > 2, "a is greater than 2", "a is less than 2")
+
+#two or more conditions 4<a<10
+a<-6
+if(a<4){"a is less than 4"}else if(a<10){"a is between 4 and 10"}else
+  {"a is greater than 10"}
+
+
+
+
+
