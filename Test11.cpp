cbcfeecae81d09b2fec11b0697a185eacfd85b2c
Harsh Rathore <91665121+Harshrathore994@users.noreply.github.com>
2022-10-19 18:42:07 +0530

    program to print right angled triangle

---
+++ Test11.cpp
@@ -0,0 +1,24 @@
+/*
+Output pattern
+* 
+* *
+* * *
+* * * *
+
+*/
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i=1; i<=4; i++)
+    {
+        for(int j=1; j<=i; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
