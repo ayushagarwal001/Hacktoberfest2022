12d045d96e505ef18e731bc8f534dd09103e1169
Unique Jain <uniquejn3004@gmail.com>
2022-10-26 19:43:50 +0530

    find the radius of circle in Java.

---
+++ Circle.java
@@ -0,0 +1,9 @@
+public class Circle{
+    public static void main(String[]args){
+        int radius;
+        double pi = 3.142,area;
+        radius = 5;
+        area = pi*radius*radius;
+        System.out.print("Area of circle is :" + area);
+    }
+}
